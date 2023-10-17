25645d85e5d4c1e95c842272d6c0c2be36c58235
angelicaw133 <angelicawo426@gmail.com>
2023-10-16 21:47:06 -0400

    k

--- dom.js
+++ dom.js
@@ -1,12 +1,12 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
-    firstname: document.querySelector(".first-name"),
+    object: document.querySelector(".object-test"),
     h2s: document.querySelectorAll("h2"),
 }
 DOMSelectors.form.addEventListener("submit", function (event){
     event.preventDefault();
-    console.log(DOMSelectors.firstname.value);
+    console.log(DOMSelectors.object.value);
     DOMSelectors.h2s.forEach(
-        (el) => el.testContent = DOMSelectors.firstname.value
+        (el) => el.testContent = DOMSelectors.object.value
     );
 });
