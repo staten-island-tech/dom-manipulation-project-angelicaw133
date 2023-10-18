f06d71be76f29a7a4563cb8962c7bc476b037895
angelicaw133 <angelicawo426@gmail.com>
2023-10-18 00:22:54 -0400

    sef

--- dom.js
+++ dom.js
@@ -2,13 +2,27 @@ const DOMSelectors = {
     form: document.querySelector("#form"),
     firstname: document.querySelector(".first_name"),
     lastname: document.querySelector(".last_name"),
-
-
+    osis: document.querySelector(".osis_number"),
+    firstnameoutput: document.querySelector("#firstnameoutput"),
+    lastnameoutput: document.querySelector("#lastnameoutput"),
+    osisoutput: document.querySelector("#osisoutput"),
 }
-DOMSelectors.form.addEventListener("submit", function (event){
+
+console.log(DOMSelectors.firstname);
+DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(DOMSelectors.firstname.value);
-    DOMSelectors.h2s.forEach(
-        (el) => el.textContent = DOMSelectors.firstname.value
-    );
+    const firstname = DOMSelectors.firstname.value
+    DOMSelectors.firstnameoutput.innertext = firstname
+});
+console.log(DOMSelectors.lastname);
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const lastname = DOMSelectors.lastname.value
+    DOMSelectors.lastnameoutput.innertext = lastname
+});
+console.log(DOMSelectors.osis);
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const osis = DOMSelectors.osis.value
+    DOMSelectors.osisoutput.innertext = osis
 });
